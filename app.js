581d04d29c5e9e6e4f8b25840cccea4da29d50e0
ikamean <ika.bidzishvili@gmail.com>
2021-03-04 05:52:51 +0400

    added express route handler for all routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -6,6 +7,6 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('build'))
-
+app.get("*", (req, res) => { res.sendFile(path.join( __dirname + "/build/index.html" )); });
 
 app.listen(PORT, () => console.log('server started on port 5001'))
